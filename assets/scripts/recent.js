c55bb0b5f3eb3aa49bd6ff41257ec75457c609d7
nabz123 <mohanr1@student.op.ac.nz>
2020-05-08 12:31:17 +1200

    changes made

--- assets/scripts/recent.js
+++ assets/scripts/recent.js
@@ -1,15 +1,18 @@
-//rently played table
-$(document).ready(function(){
-    $.getJSON("../../recent.json", function(data){ 
-       var tracks_data = '';
-       $.each(data, function(key, value){
-        tracks_data += '<tr class="tracks">';
-        tracks_data += '<td class="track_btn"><a href="'+value.link+'" target="_blank"><i class="far fa-play-circle"></i></a></td>';
-        tracks_data += '<td><img class="track_img" src='+value.img+'></td>';
-        tracks_data += '<td class="track_detail2"><b style="font-size: 15px;">'+value.title+'<td class="track_detail1"><b style="font-size: 15px;">'+value.artist+'</span></td>';
-        tracks_data += '<td class="track_played">'+value.played+' Minutes ago</td>';
-        tracks_data += '</tr>';
-       });
-       $('#recent_table').append(tracks_data); //append to #recent_table
-    });
- });
+fetch("../../recent.json").then(
+   res=>{
+       res.json().then(
+           data=>{
+            var tracks_data = '';
+                data.forEach((value) => {             
+                    tracks_data += '<tr class="tracks">';
+                    tracks_data += '<td><a href="'+value.link+'" target="_blank"><img class="track_src" src='+value.src+' width="2px"></td>';
+                    tracks_data += '<td><img class="track_img" src='+value.img+'></td>';
+                    tracks_data += '<td class="track_detail2"><b style="font-size: 15px;">'+value.title+'<td class="track_detail1"><b style="font-size: 15px;">'+value.artist+'</span></td>';
+                    tracks_data += '<td class="track_played">'+value.played+' Minutes ago</td>';        
+                });
+               document.getElementById('recent_table').innerHTML = tracks_data;
+               console.log(data);
+            }
+        )
+    }
+)
--- assets/scripts/toptrack.js
+++ assets/scripts/toptrack.js
@@ -1,21 +1,23 @@
-//Top Tracks Table
-$(document).ready(function(){
-    $.getJSON("../../toptracks.json", function(data){
-       let tracks_data = '';
-       $.each(data, function(key, value){
-         tracks_data += '<tr class="tracks">'; 
-         tracks_data += '<td class="track_id">'+value.id+'</td>'; //fetches song id from json file
-         tracks_data += '<td class="track_btn"><a href="'+value.link+'" target="_blank"><i class="far fa-play-circle"></i></a></td>'; //fetches play button from library 
-         tracks_data += '<td><img class="track_img" src='+value.img+'></td>'; //fetches image from json file
-         tracks_data += '<td class="track_detail"><b style="font-size: 15px">'+value.Title+'</b><br><span style="font-size: 13px">'+value.artist+'</span></td>'; 
-         tracks_data += '<td class="track_count"><span class="chartlist-count-bar-slug" style="width:'+value.plays+'%">'+value.plays+' Plays</span></td>';
-         tracks_data += '</tr>';
-      });
-      $('#tracks_table').append(tracks_data); //append to div with the id tracks_table
-   });
-});
-
-//Heart functionality
+fetch("../../toptracks.json").then(
+   res=>{
+       res.json().then(
+           data=>{
+            var tracks_data = '';
+               data.forEach((value) => {
+                  tracks_data += '<tr class="tracks">';
+                  tracks_data += '<td class="track_id">'+value.id+'</td>';
+                  tracks_data += '<td><a href="'+value.link+'" target="_blank"><img class="track_src" src='+value.src+'></td>';
+                  tracks_data += '<td><img class="track_img" src='+value.img+'></td>';
+                  tracks_data += '<td class="track_detail"><b style="font-size: 15px">'+value.Title+'</b><br><span style="font-size: 13px">'+value.artist+'</span></td>';
+                  tracks_data += '<td class="track_count"><span class="chartlist-count-bar-slug" style="width:'+value.plays+'%">'+value.plays+' Listeners</span></td>';
+                  tracks_data += '</tr>';                  
+               });
+            document.getElementById('tracks_table').innerHTML = tracks_data;
+            console.log(data);
+         }
+      )
+   }
+)
 document.addEventListener("click",function(event){
 if (event.target.matches(".heartlove"))
 {
@@ -28,3 +30,4 @@ else if(event.target.matches(".heartunolve"))
    event.target.classList.add("heartlove");
    }
 });
+    
