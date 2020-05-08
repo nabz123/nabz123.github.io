37926f99aa7ec3e11b6b2db198c263e02c9e421a
nabz123 <mohanr1@student.op.ac.nz>
2020-05-08 12:27:52 +1200

    more corrections made

--- assets/scripts/topArtists.js
+++ assets/scripts/topArtists.js
@@ -1,15 +1,11 @@
-fetch("../../topArtists.json").then(
-    res=>{
-        res.json().then(
-            data=>{
-                var element ="";
-                data.forEach((item) => {
-                    element += '<div class="xxx"><img class="imgflex" src="' + item.src + '"><p class="imageHead">'
-                    + item.name+'</p> <p class="imageText">' + item.plays+ '</p></div>';                  
-                });
-                document.getElementById("artist_container").innerHTML = element;
-                console.log(data);
-            }
-        )
-    }
-)
+//top artist table
+$(document).ready(function() {
+    $.getJSON("../../topArtists.json", function(data){
+    data.forEach(function(item) {
+        console.log("top artist")
+        var element = $('<div><img class="imgflex" src="' + item.src + '"><p class="imageHead">' 
+        + item.name+'</p> <p class="imageText">' + item.plays+ '</p></div>');
+        $("#artist_container").append(element); //append to #artist_container
+    });
+    });
+})
